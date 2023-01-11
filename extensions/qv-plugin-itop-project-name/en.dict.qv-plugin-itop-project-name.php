9ffa2b184626ead08009ea86345aecbd75c5360d
Your <you@example.com>
2023-01-11 12:13:20 -0500

    Se agrego un nuevo campo en el portal de usuario

--- extensions/qv-plugin-itop-project-name/en.dict.qv-plugin-itop-project-name.php
+++ extensions/qv-plugin-itop-project-name/en.dict.qv-plugin-itop-project-name.php
@@ -8,5 +8,9 @@
 
 Dict::Add('EN US', 'English', 'English', array(
 	// Dictionary entries go here
+	'Class:Ticket/Attribute:project_name' => 'Project Name',
+	'Class:Ticket/Attribute:project_name+' => 'Project Name',
+	'Class:Ticket/Attribute:company_acronym' => 'Company Acronym',
+	'Class:Ticket/Attribute:company_acronym+' => 'Company Acronym',
 ));
 ?>
---
+++ extensions/qv-plugin-itop-project-name/es_cr.dict.qv-plugin-itop-project-name.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('ES CR', 'Español', 'Español , Castellano', array(
+	// Dictionary entries go here
+	'Class:Ticket/Attribute:project_name' => 'Nombre Proyecto',
+	'Class:Ticket/Attribute:project_name+' => 'Nombre Proyecto',
+	'Class:Ticket/Attribute:company_acronym' => 'Acronimo Compañia',
+	'Class:Ticket/Attribute:company_acronym+' => 'Acronimo Compañia',
+));
+?>
